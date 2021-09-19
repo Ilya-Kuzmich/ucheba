410dc3ccd846af0a7ccdaf505e42f0ede6c27a36
ilia <iliakuzmich80@gmail.com>
2021-09-19 15:15:19 +0300

    gg

--- ff.js
+++ ff.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 fs.emptyDir('nomer1')
 fs.emptyDir('nomer2')
 .then(() => {
-console.log('dve papki!')
+console.log('dve_papki!')
   fs.writeFile('D:/u4eba/112/nomer1/file1.txt', 'glhf')
   .then(() => {
   console.log('file#1!')
