2b708bc6723e40beed144140cc26bcb3d5b427ce
ilia <iliakuzmich80@gmail.com>
2021-09-19 14:30:16 +0300

    main

---
+++ ff.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra')
+fs.emptyDir('nomer1')
+fs.emptyDir('nomer2')
+.then(() => {
+console.log('papki!')
+  fs.writeFile('D:/u4eba/112/nomer1/file1.txt', 'glhf')
+  .then(() => {
+  console.log('file!')
+fs.copyFile('D:/u4eba/112/nomer1/file1.txt','D:/u4eba/112/nomer2/file1.txt')
+  .then(() => {
+    console.log('copy!')  
+    fs.remove('D:/u4eba/112/nomer1')
+ fs.remove('D:/u4eba/112/nomer2')
+ .then(() => {
+  console.log('delete!')
+})
+  .catch(err => {
+    console.error(err)      
+})
+
+})
+})
+})
