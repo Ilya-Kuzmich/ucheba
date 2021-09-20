535a9179623e9fc252f46b2a0444e5bef93b9a80
ilia <iliakuzmich80@gmail.com>
2021-09-20 13:25:45 +0300

    new

--- ff.js
+++ ff.js
@@ -1,16 +1,16 @@
 const fs = require('fs-extra')
-fs.emptyDir('nomer1')
-fs.emptyDir('nomer2')
+fs.emptyDir('w211')
+fs.emptyDir('w212')
 .then(() => {
 console.log('dve_papki!')
-  fs.writeFile('D:/u4eba/112/nomer1/file1.txt', 'dota2glhf')
+  fs.writeFile('D:/u4eba/112/w211/om645.txt', '2.2tdi 170ls')
   .then(() => {
   console.log('file#1!')
-fs.copyFile('D:/u4eba/112/nomer1/file1.txt','D:/u4eba/112/nomer2/file1.txt')
+fs.copyFile('D:/u4eba/112/w211/om645.txt','D:/u4eba/112/w212/om645.txt')
   .then(() => {
     console.log('copy!')  
-    fs.remove('D:/u4eba/112/nomer1')
- fs.remove('D:/u4eba/112/nomer2')
+    fs.remove('D:/u4eba/112/w211')
+ fs.remove('D:/u4eba/112/w212')
  .then(() => {
   console.log('delete!')
 })
