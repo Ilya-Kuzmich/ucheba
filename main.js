4107ac6211ed684bde334d222644ce93dd5f4853
ilia <iliakuzmich80@gmail.com>
2021-09-25 12:56:02 +0300

    25.09

---
+++ main.js
@@ -0,0 +1,16 @@
+let x=15
+console.log("3"+x);
+console.log(x+true);
+console.log("4"+true);
+
+console.log("14"*x);
+console.log(x*true);
+console.log("1"*true);
+
+console.log("100"/x);
+console.log(x/true);
+console.log("12"/true);
+
+console.log(String(x));
+console.log(Boolean(x));
+console.log(Number(false));
