80b63e2e912730bed95aa47b4e23d65ff0277b1b
ilia <iliakuzmich80@gmail.com>
2021-10-07 12:16:28 +0300

    07/10

--- dz_les7.js
+++ dz_les7.js
@@ -2,13 +2,15 @@
 function t1() {
 let arr = [1,2,3,4,5,6];
 let arr1 = [6,5,4,3,2,1];
+console.log('поменять массив в обратном порядке- [1,2,3,4,5,6] [6,5,4,3,2,1]')
 console.log(arr.reverse(),arr1.reverse())
 }
 t1();
 // найти максимальное значение числа в массиве 
 function t2() {
     let arr = [1, 2, 3, 8, 5, 6];
-    console.log(arr.reduce((a, b) => a > b ? a : b));
+    console.log('найти максимальное значение числа в массиве')
+    console.log(arr.reduce((a, b) => a > b ? a : b))
 };
 t2();
 //  записать в массив ряд фибаначи начиная с N члена с длинной массива M
@@ -16,23 +18,26 @@ function t3() {
     let n = 5, m = 10, a = 0, b = 1;
     let fArr = [0, 1];
     new Array(m).fill().forEach((el, index) => {
-        a = fArr[index];
-        b = fArr[index + 1];
-        fArr.push(a + b);
+        a = fArr[index]
+        b = fArr[index + 1]
+        fArr.push(a + b)
     })
-    console.log(fArr.slice(n - 1, m));
+    console.log('записать в массив ряд фибаначи начиная с N члена с длинной массива M')
+    console.log(fArr.slice(n - 1, m))
 };
 t3();
 // сортировка массива по возрастанию/убыванию
 function t5() {
-    let arr = [2,3,5,1,4]
-    console.log(arr.sort());
-    console.log(arr.reverse(arr.sort));
+    let arr = [2,3,5,1,4];
+    console.log('сортировка массива по возрастанию/убыванию')
+    console.log(arr.sort())
+    console.log(arr.reverse(arr.sort))
 };
 t5();
 //удалить из массива все повторяющиеся элементы
 function t6() {
-    let arr = [1,2,3,3,4,2,4,5];
-    console.log(arr.filter(function (item, index) { return arr.indexOf(item) === index; }));
+    let arr = [1,2,3,3,4,2,5];
+    console.log('удалить из массива все повторяющиеся элементы')
+    console.log(arr.filter(function (item, index) { return arr.indexOf(item) === index; }))
 };
 t6();
