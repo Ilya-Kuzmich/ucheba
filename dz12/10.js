3b332861a3af1abcc1f870f3d65fe24453bedb44
ilia <iliakuzmich80@gmail.com>
2021-10-15 14:41:28 +0300

    v

--- dz12/10.js
+++ dz12/10.js
@@ -26,15 +26,49 @@ const nomer1 = 'nomer1';
 const nomer2 = 'nomer2';
 const file1 = 'nomer1/file1.txt';
 
-function papka1(nomer1){ 
-fs.emptyDir(nomer1, (err) => {
-  if (err) return console.error(err)
-  console.log('papka #1');
-  papka1(nomer1)
-})
+function papka1(nomer1) {
+    fs.emptyDir(nomer1, (err) => {
+        if (err) return console.error(err);
+        console.log('papka1');
+        File1(file1);
+    });
 }
 
-fs.copy('/tmp/myfile', '/tmp/mynewfile', err => {
-  if (err) return console.error(err)
-  console.log('success!')
-})
+function File1(file1) {
+    fs.ensureFile(file1, (err) => {
+        if (err) return console.error(err);
+        console.log('file1');
+        papka2(nomer2);
+    });
+}
+
+function papka2(nomer2) {
+    fs.emptyDir(nomer2, (err) => {
+        if (err) return console.error(err);
+        console.log('papka2');
+        moveFile(file1, file2);
+    });
+}
+
+function moveFile(file1, file2) {
+    fs.move(file1, file2, (err) => {
+        if (err) return console.error(err);
+        console.log('moved');
+        deleteFile(file2);
+    });
+}
+
+function deleteFile(file2) {
+    fs.remove(file2, (err) => {
+        if (err) return console.error(err);
+        console.log('file removed');
+        deleteFolders(nomer1, nomer2);
+    });
+}
+
+function deleteFolders(momer1, nomer2) {
+    fs.remove(momer1);
+    fs.remove(nomer2);
+    console.log('papki removed');
+}
+papka1(nomer1);
